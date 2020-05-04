94f3013eea17643680303cd80438dc2da6b5a73b
Xinsong Li <lixinso@gmail.com>
2020-05-04 11:48:56 -0500

    install_llvm_on_jetson.sh

--- install_llvm_on_jetson.sh
+++ install_llvm_on_jetson.sh
@@ -3,12 +3,13 @@
 wget http://releases.llvm.org/7.0.1/llvm-7.0.1.src.tar.xz
 tar -xvf llvm-7.0.1.src.tar.xz
 cd llvm-7.0.1.src
-mkdir llvm_build_dir
+mkdir -p llvm_build_dir
 cd llvm_build_dir/
 cmake ../ -DCMAKE_BUILD_TYPE=Release                     -DLLVM_TARGETS_TO_BUILD="ARM;X86;AArch64"
 make -j4 
 cd bin/
 echo "export LLVM_CONFIG=\""`pwd`"/llvm-config\"" >> ~/.bashrc
 echo "alias llvm='"`pwd`"/llvm-lit'" >> ~/.bashrc
-source ~/.bashrc && pip3 install llvmlite
+
+LLVM_CONFIG="/myworkdir/llvm-7.0.1.src/llvm_build_dir/bin/llvm-config" pip3 install llvmlite
 
